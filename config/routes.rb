0e6b70012c7bc3862b18c8f33dda453f11f5c566
kondor <ociepkakonrad@gmail.com>
2017-08-16 15:05:24 +0200

    create layout and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#index'
+  
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :users, only: :create
+  resources :sessions, only: :create
+  
+  resources :events
+  resources :competitors
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816121951) do
+ActiveRecord::Schema.define(version: 20170816125326) do
 
   create_table "competitors", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 20170816121951) do
     t.datetime "updated_at",    null: false
     t.index ["competitor_id"], name: "index_scores_on_competitor_id"
     t.index ["list_id"], name: "index_scores_on_list_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
