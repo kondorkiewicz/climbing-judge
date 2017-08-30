cc2206a15b95c77f445bf237552370f6764365ae
kondorkiewicz <ociepkakonrad@gmail.com>
2017-08-30 10:51:55 +0200

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_climbing_judge2_session'
+Rails.application.config.session_store :cookie_store, key: '_climbing_judge_session'
