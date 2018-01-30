7b51e5a060616a1372b7f29353ddc26f92b2805e
Konrad Ociepka <ociepkakonrad@gmail.com>
2018-01-30 15:25:08 +0100

    new RESTful routes for rounds

--- app/controllers/eliminations_controller.rb
+++ app/controllers/eliminations_controller.rb
@@ -2,37 +2,37 @@ class EliminationsController < ApplicationController
   before_action :set_event, :authorize, :authorize_event, :check_finished_status
   before_action except: [:create_eliminations_lists] { check_status('eliminations') }
   before_action only: [:create_eliminations_lists] { check_status('competitors') }
-  
-  def eliminations   
+
+  def show
       @m1 = @event.list('first_route', 'men')
       @f1 = @event.list('first_route', 'women')
       @m2 = @event.list('second_route', 'men')
       @f2 = @event.list('second_route', 'women')
-  end 
-  
-  def create_eliminations_lists 
+  end
+
+  def create
     if enough_competitors?
       @event.create_eliminations_lists
-      @event.update_attribute(:status, 'eliminations') 
-      redirect_to eliminations_event_path, 
+      @event.update_attribute(:status, 'eliminations')
+      redirect_to eliminations_event_path,
         info: "Eliminations lists have been created."
-    else 
-      redirect_to competitors_event_path, 
+    else
+      redirect_to competitors_event_path,
         danger: "There should be at least two competitors in each category!"
-    end 
-  end 
-  
-  def delete_eliminations_lists
+    end
+  end
+
+  def destroy
     @event.eliminations_lists.destroy_all
     @event.update_attribute(:status, 'competitors')
-    redirect_to competitors_event_path, 
+    redirect_to competitors_event_path,
       info: 'Eliminations lists have been deleted.'
   end
-  
-  private 
-  
+
+  private
+
   def enough_competitors?
     @event.men.size >= 2 && @event.women.size >= 2
-  end 
-  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,31 +6,10 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy', as: 'logout'
   resources :users, only: :create
   resources :sessions, only: :create
-  
-  resources :events do 
-    member do 
-      get 'competitors', controller: 'event_competitors'
-      get 'delete_competitor', controller: 'event_competitors'
-      get 'add_competitor', controller: 'event_competitors'
-      get 'create_eliminations_lists', controller: 'eliminations'
-      get 'eliminations', controller: 'eliminations'
-      get 'compute_eliminations_results', controller: 'eliminations_results'
-      get 'eliminations_results', to: 'eliminations_results#index'
-      get 'create_semi_finals_lists', controller: 'semi_finals'
-      get 'create_finals_lists', controller: 'finals'
-      get 'semi_finals', controller: 'semi_finals'
-      get 'finals', controller: 'finals'
-      get 'delete_eliminations_lists', controller: 'eliminations'
-      get 'delete_eliminations_results', controller: 'eliminations_results'
-      get 'delete_semi_finals_lists', controller: 'semi_finals'
-      get 'delete_finals_lists', controller: 'finals'
-      get 'compute_results', controller: 'results'
-    end 
-  end 
-  
-  resources :scores, only: :update
-  resources :lists, only: :show
-  resources :competitors
-  
-  get '*path' => redirect('/')
+
+  resources :events, except: [:edit, :update] do
+    resource :eliminations, only: [:show, :create, :destroy]
+    resource :semi_finals, only: [:show, :create, :destroy]
+    resource :finals, only: [:show, :create, :destroy]
+  end
 end
